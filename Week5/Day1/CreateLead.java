2fa3b409ac4bc38ad103f9611005e58c79199c78
testleaftrainings <136911461+testleaftrainings@users.noreply.github.com>
2024-10-10 00:25:15 +0530

    Week5_Day1

---
+++ Week5/Day1/CreateLead.java
@@ -0,0 +1,41 @@
+package testcases2;
+
+import java.io.IOException;
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+
+
+public class CreateLead  {
+
+
+	@Test
+    public void runCreateLead() {
+		ChromeDriver driver=new ChromeDriver();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/control/main");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.findElement(By.id("username")).sendKeys("Demosalesmanager");
+		driver.findElement(By.id("password")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("TestLeaf");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Vineeth");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Rajendran");
+		driver.findElement(By.name("submitButton")).click();
+		
+}
+}
+
+
+
+
+
+
